bcf0af84e48c53464f5a6255a7ddfb32e8dbc42c
zhangdanji <zhangdanji@chezhibao.com>
2018-06-29 10:03:24 +0800

    iot集成kafka

--- lora-netty/src/test/java/com/jsptpd/netty/lora/LoraNettyApplicationTests.java
+++ lora-netty/src/test/java/com/jsptpd/netty/lora/LoraNettyApplicationTests.java
@@ -1,10 +1,13 @@
 package com.jsptpd.netty.lora;
 
-import com.jsptpd.netty.client.NettyClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,7 +15,7 @@ public class LoraNettyApplicationTests {
 
     @Test
     public void contextLoads() {
-        /*try {
+        try {
             Thread.sleep(3000);
             Socket socket = new Socket("localhost",18000);
             String message = "{\"data\":\"test\"}";
@@ -22,17 +25,17 @@ public class LoraNettyApplicationTests {
             out.flush();
             String line;
             while((line = reader.readLine()) != null){
-                System.out.println("接受消息:" + line);
+                System.out.println("---接受消息服务端:" + line);
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
-        try {
+        }
+        /*try {
             Thread.sleep(3000);
             NettyClient.sendString("数据发送自客户端");
             Thread.sleep(Integer.MAX_VALUE);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
